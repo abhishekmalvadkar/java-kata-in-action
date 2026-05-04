05ded30bbf81d41d31345daf87cbbc4903c3f670
Abhishek <abhi@dev.com>
2026-05-04 11:56:58 +0530

    just refactoring test

--- fizz-buzz-kata/src/test/java/com/amalvadkar/fbk/FizzBuzzParserTest.java
+++ fizz-buzz-kata/src/test/java/com/amalvadkar/fbk/FizzBuzzParserTest.java
@@ -1,26 +1,31 @@
 package com.amalvadkar.fbk;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzParserTest {
 
+    FizzBuzzParser fizzBuzzParser;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzzParser = new FizzBuzzParser();
+    }
+
     @Test
     void should_return_1_when_passed_input_as_1() {
-        FizzBuzzParser fizzBuzzParser = new FizzBuzzParser();
         assertThat(fizzBuzzParser.parse(1)).isEqualTo("1");
     }
 
     @Test
     void should_return_2_when_passed_input_as_1() {
-        FizzBuzzParser fizzBuzzParser = new FizzBuzzParser();
         assertThat(fizzBuzzParser.parse(2)).isEqualTo("2");
     }
 
     @Test
     void should_return_4_when_passed_input_as_4() {
-        FizzBuzzParser fizzBuzzParser = new FizzBuzzParser();
         assertThat(fizzBuzzParser.parse(4)).isEqualTo("4");
     }
 }
