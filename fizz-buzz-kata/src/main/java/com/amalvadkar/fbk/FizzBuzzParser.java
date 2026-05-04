4ac78a16f88d4b192a11cabb7e0db6db02bf1c75
Abhishek <abhi@dev.com>
2026-05-04 11:52:16 +0530

    just refactoring

--- fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
+++ fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
@@ -3,9 +3,6 @@ package com.amalvadkar.fbk;
 public class FizzBuzzParser {
 
     public String parse(int input) {
-        if (input == 1) return "1";
-        if (input == 2) return "2";
-        if (input == 4) return "4";
-        return "";
+        return String.valueOf(input);
     }
 }
