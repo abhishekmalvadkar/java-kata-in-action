3e34fdc75a3056fba0ca5451e621372a974794db
Abhishek <abhi@dev.com>
2026-05-05 11:42:21 +0530

    Implemented IntelliJ run configuration to run all fizz buzz kata related tests

--- fizz-buzz-kata/src/test/java/com/amalvadkar/fbk/FizzBuzzParserTest.java
+++ fizz-buzz-kata/src/test/java/com/amalvadkar/fbk/FizzBuzzParserTest.java
@@ -1,10 +1,12 @@
 package com.amalvadkar.fbk;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Tag("fizz-buzz")
 public class FizzBuzzParserTest {
 
     FizzBuzzParser fizzBuzzParser;
