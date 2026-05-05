6bd39e9cda2cabe082bad0c9659f25e0afb759e8
Abhishek <abhi@dev.com>
2026-05-05 11:32:03 +0530

    just refactoring

--- fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
+++ fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
@@ -1,11 +1,29 @@
 package com.amalvadkar.fbk;
 
+import org.jspecify.annotations.NonNull;
+
 public class FizzBuzzParser {
 
-    public String parse(int input) {
-        if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
-        if (input % 3 == 0) return "Fizz";
-        if (input % 5 == 0) return "Buzz";
-        return String.valueOf(input);
+    public String parse(int number) {
+        if (isDivisibleBy3And5(number)) return "FizzBuzz";
+        if (isDivisibleBy3(number)) return "Fizz";
+        if (isDivisibleBy5(number)) return "Buzz";
+        return itSelfAsString(number);
+    }
+
+    private static @NonNull String itSelfAsString(int number) {
+        return String.valueOf(number);
+    }
+
+    private static boolean isDivisibleBy3And5(int number) {
+        return isDivisibleBy3(number) && isDivisibleBy5(number);
+    }
+
+    private static boolean isDivisibleBy5(int input) {
+        return input % 5 == 0;
+    }
+
+    private static boolean isDivisibleBy3(int input) {
+        return input % 3 == 0;
     }
 }
