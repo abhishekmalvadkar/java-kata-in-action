afaf875f97f80c128a31adc297521e7c221e7ec9
Abhishek <abhi@dev.com>
2026-05-05 11:34:28 +0530

    Minimized visibility

--- fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
+++ fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
@@ -4,9 +4,9 @@ import org.jspecify.annotations.NonNull;
 
 public class FizzBuzzParser {
 
-    public static final String FIZZ = "Fizz";
-    public static final String BUZZ = "Buzz";
-    public static final String FIZZ_BUZZ = FIZZ + BUZZ;
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZ_BUZZ = FIZZ + BUZZ;
 
     public String parse(int number) {
         if (isDivisibleBy3And5(number)) return FIZZ_BUZZ;
