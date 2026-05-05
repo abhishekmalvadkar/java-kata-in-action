4e6e3deb6b48e8b1882b1ac669298783076fc343
Abhishek <abhi@dev.com>
2026-05-05 11:23:49 +0530

    Move ahead from triangulation

--- fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
+++ fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
@@ -3,8 +3,7 @@ package com.amalvadkar.fbk;
 public class FizzBuzzParser {
 
     public String parse(int input) {
-        if (input == 5) return "Buzz";
-        if (input == 10) return "Buzz";
+        if (input % 5 == 0) return "Buzz";
         if (input % 3 == 0) return "Fizz";
         return String.valueOf(input);
     }
