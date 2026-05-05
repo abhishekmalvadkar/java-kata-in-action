6bcaa2d68c929ad22adcfbb883b3762edbca5229
Abhishek <abhi@dev.com>
2026-05-05 11:34:01 +0530

    Let's get rid of magic strings

--- fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
+++ fizz-buzz-kata/src/main/java/com/amalvadkar/fbk/FizzBuzzParser.java
@@ -4,14 +4,18 @@ import org.jspecify.annotations.NonNull;
 
 public class FizzBuzzParser {
 
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZ_BUZZ = FIZZ + BUZZ;
+
     public String parse(int number) {
-        if (isDivisibleBy3And5(number)) return "FizzBuzz";
-        if (isDivisibleBy3(number)) return "Fizz";
-        if (isDivisibleBy5(number)) return "Buzz";
+        if (isDivisibleBy3And5(number)) return FIZZ_BUZZ;
+        if (isDivisibleBy3(number)) return FIZZ;
+        if (isDivisibleBy5(number)) return BUZZ;
         return itSelfAsString(number);
     }
 
-    private static @NonNull String itSelfAsString(int number) {
+    private static String itSelfAsString(int number) {
         return String.valueOf(number);
     }
 
