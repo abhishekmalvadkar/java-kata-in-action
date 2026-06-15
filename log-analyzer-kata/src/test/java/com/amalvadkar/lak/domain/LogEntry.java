48936b367b01fa0c0cdc003825340016124c249f
Abhishek <abhi@dev.com>
2026-06-15 12:17:31 +0530

    Refactoring with factory method

--- log-analyzer-kata/src/test/java/com/amalvadkar/lak/domain/LogEntry.java
+++ log-analyzer-kata/src/test/java/com/amalvadkar/lak/domain/LogEntry.java
@@ -7,7 +7,15 @@ import java.time.LocalDateTime;
 
 @Data
 public class LogEntry {
-    private LocalDateTime timestamp;
-    private LogLevel logLevel;
-    private String message;
+    private final LocalDateTime timestamp;
+    private final LogLevel logLevel;
+    private final String message;
+
+    public static LogEntry from(String timeStamp, String logLevel, String message) {
+        return new LogEntry(
+                LocalDateTime.parse(timeStamp),
+                LogLevel.valueOf(logLevel),
+                message
+        );
+    }
 }
--- log-analyzer-kata/src/test/java/com/amalvadkar/lak/parser/LogParser.java
+++ log-analyzer-kata/src/test/java/com/amalvadkar/lak/parser/LogParser.java
@@ -1,9 +1,6 @@
 package com.amalvadkar.lak.parser;
 
 import com.amalvadkar.lak.domain.LogEntry;
-import com.amalvadkar.lak.enums.LogLevel;
-
-import java.time.LocalDateTime;
 
 public class LogParser {
     public LogEntry parse(String singleLogEntry) {
@@ -11,10 +8,6 @@ public class LogParser {
         String timeStamp = logEntryParts[0];
         String logLevel = logEntryParts[1];
         String message = logEntryParts[2];
-        LogEntry logEntry = new LogEntry();
-        logEntry.setTimestamp(LocalDateTime.parse(timeStamp));
-        logEntry.setLogLevel(LogLevel.valueOf(logLevel));
-        logEntry.setMessage(message);
-        return logEntry;
+        return LogEntry.from(timeStamp, logLevel, message);
     }
 }
