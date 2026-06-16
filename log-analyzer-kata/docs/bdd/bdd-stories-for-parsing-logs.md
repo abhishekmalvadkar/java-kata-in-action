dd20e7cb21a0c716c4326ef203c62b13718ffdca
Abhishek <abhi@dev.com>
2026-06-16 11:35:16 +0530

    * Scenario:   * Parse single log entry with multi word log message * Given:   * Log entry: "2026-07-10T10:00:00 INFO : User login" * When:   * We call parse() method on LogParser * Then:   * It should give LogEntry object which should have below things:     * Timestamp : 2026-07-10T10:00:00     * Log level : INFO     * Message : User login

--- log-analyzer-kata/docs/bdd/bdd-stories-for-parsing-logs.md
+++ log-analyzer-kata/docs/bdd/bdd-stories-for-parsing-logs.md
@@ -10,4 +10,16 @@
   * It should give LogEntry object which should have below things:
     * Timestamp : 2026-07-10T10:00:00
     * Log level : INFO
+    * Message : login
+
+* Scenario:
+  * Parse single log entry with multi word log message
+* Given:
+  * Log entry: "2026-07-10T10:00:00 INFO : User login"
+* When:
+  * We call parse() method on LogParser
+* Then:
+  * It should give LogEntry object which should have below things:
+    * Timestamp : 2026-07-10T10:00:00
+    * Log level : INFO
     * Message : User login
--- log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogParserTest.java
+++ log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogParserTest.java
@@ -18,4 +18,15 @@ public class LogParserTest extends AbstractLogAnalyzerTest {
         assertThat(logEntry.getLogLevel()).isEqualTo(LogLevel.INFO);
         assertThat(logEntry.getMessage()).isEqualTo("login");
     }
+
+    @Test
+    void should_parse_single_log_entry_with_multi_word_log_message() {
+        LogParser logParser = new LogParser();
+
+        LogEntry logEntry = logParser.parse("2026-07-10T10:00:00 INFO : User login");
+
+        assertThat(logEntry.getTimestamp()).isEqualTo("2026-07-10T10:00:00");
+        assertThat(logEntry.getLogLevel()).isEqualTo(LogLevel.INFO);
+        assertThat(logEntry.getMessage()).isEqualTo("User login");
+    }
 }
