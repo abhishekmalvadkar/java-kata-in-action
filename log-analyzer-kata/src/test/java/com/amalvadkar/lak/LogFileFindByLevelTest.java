13d8b76fb868c6e669893afc7d76361014c2fe24
Abhishek <abhi@dev.com>
2026-06-19 11:57:31 +0530

    Some other test cases for find by log level

--- log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogFileFindByLevelTest.java
+++ log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogFileFindByLevelTest.java
@@ -23,4 +23,35 @@ public class LogFileFindByLevelTest extends AbstractLogAnalyzerTest {
           LogEntry.from("2026-07-10T10:02:00", "INFO", "User logout")
         );
     }
+
+    @Test
+    void should_return_entries_by_error_log_level() {
+        String logEntries = """
+                2026-07-10T10:00:00 INFO : User login
+                2026-07-10T10:01:00 ERROR : Payment failed
+                2026-07-10T10:02:00 ERROR : Database unavailable
+                """;
+        LogFile logFile = LogFile.from(logEntries);
+
+        assertThat(logFile.findEntriesByLevel(LogLevel.ERROR)).containsExactly(
+                LogEntry.from("2026-07-10T10:01:00", "ERROR", "Payment failed"),
+                LogEntry.from("2026-07-10T10:02:00", "ERROR", "Database unavailable")
+        );
+    }
+
+    @Test
+    void should_return_empty_collection_when_asked_entries_does_not_exists_for_give_log_level() {
+        String logEntries = "2026-07-10T10:00:00 INFO : User login";
+        LogFile logFile = LogFile.from(logEntries);
+
+        assertThat(logFile.findEntriesByLevel(LogLevel.ERROR)).isEmpty();
+    }
+
+    @Test
+    void should_return_empty_collection_when_asked_entries_by_log_level_on_empty_file() {
+        String logEntries = "";
+        LogFile logFile = LogFile.from(logEntries);
+
+        assertThat(logFile.findEntriesByLevel(LogLevel.ERROR)).isEmpty();
+    }
 }
