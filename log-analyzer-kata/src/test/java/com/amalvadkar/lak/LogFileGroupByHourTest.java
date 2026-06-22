3785b28510d70ad41fc4a89a7e6a73ee2b4e5065
Abhishek <abhi@dev.com>
2026-06-22 11:20:17 +0530

    Some other test cases for group entry count by hour

--- log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogFileGroupByHourTest.java
+++ log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogFileGroupByHourTest.java
@@ -24,4 +24,25 @@ public class LogFileGroupByHourTest extends AbstractLogAnalyzerTest {
                         "2026-07-10T11", 2L)
         );
     }
+
+    @Test
+    void should_return_hour_with_entry_count_when_all_entries_happen_at_same_hour_and_ask_for_group_by_hour() {
+        String logEntries = """
+                2026-07-10T10:00:00 INFO : User login
+                2026-07-10T10:30:00 INFO : User logout
+                """;
+        LogFile logFile = LogFile.from(logEntries);
+
+        assertThat(logFile.groupByHour()).containsExactlyInAnyOrderEntriesOf(
+                Map.of("2026-07-10T10", 2L)
+        );
+    }
+
+    @Test
+    void should_return_empty_map_when_ask_for_group_by_hour_on_empty_log_files() {
+        String logEntries = "";
+        LogFile logFile = LogFile.from(logEntries);
+
+        assertThat(logFile.groupByHour()).isEmpty();
+    }
 }
