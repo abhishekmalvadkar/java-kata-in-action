eb648ff2cdbc1e64cf511b0259828343f066f0fb
Abhishek <abhi@dev.com>
2026-06-23 11:27:58 +0530

    Some other test cases for entries between time range

--- log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogFileEntriesBetweenTest.java
+++ log-analyzer-kata/src/test/java/com/amalvadkar/lak/LogFileEntriesBetweenTest.java
@@ -26,4 +26,27 @@ public class LogFileEntriesBetweenTest extends AbstractLogAnalyzerTest {
                 2026-07-10T10:45:12 ERROR : Payment failed
                 2026-07-10T11:05:10 INFO : User logout""");
     }
+
+    @Test
+    void should_return_empty_string_when_not_entries_exists_for_asked_date_time_range() {
+        String logEntries = """
+                2026-07-10T10:00:00 INFO : User login
+                2026-07-10T10:15:30 WARN : High memory usage
+                """;
+        LogFile logFile = LogFile.from(logEntries);
+
+        LocalDateTime start = LocalDateTime.of(2026, 7, 10, 11, 0);
+        LocalDateTime end = LocalDateTime.of(2026, 7, 10, 12, 0);
+        assertThat(logFile.entriesBetween(start, end)).isEmpty();
+    }
+
+    @Test
+    void should_return_empty_string_when_entries_asked_with_date_time_range_on_empty_log_file() {
+        String logEntries = "";
+        LogFile logFile = LogFile.from(logEntries);
+
+        LocalDateTime start = LocalDateTime.of(2026, 7, 10, 11, 0);
+        LocalDateTime end = LocalDateTime.of(2026, 7, 10, 12, 0);
+        assertThat(logFile.entriesBetween(start, end)).isEmpty();
+    }
 }
